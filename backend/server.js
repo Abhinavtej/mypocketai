c0a560cb6bdd883acc55338d363a581357e6eab8
Abhinavtej Reddy <87389936+Abhinavtej@users.noreply.github.com>
2024-11-16 00:54:36 +0530

    server2

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,13 @@ const incomeRoutes = require('./routes/income');
 const budgetRoutes = require('./routes/budget');
 const expenseRoutes = require('./routes/expense');
 const reportsRoutes = require('./routes/reports');
-const allowedOrigins = ['https://mypocketai.azurewebsites.com'];
 
 dotenv.config();
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-  origin: allowedOrigins
-}));
+app.use(cors());
 
 // Routes
 app.use("/api/auth", authRoutes);
@@ -32,7 +29,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .catch((err) => console.log(err));
 
 // Start the server
-app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
-  console.log('Server is running on https://mypocketai.azurewebsites.com');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
-
